1e779b5d3ab9099a84e1e1c3261c2f92ec0bb707
ZennGeeraerts <76271773+ZennGeeraerts@users.noreply.github.com>
2021-10-19 00:05:28 +0200

    Update SmileEngine.cpp

--- SmileEngine.cpp
+++ SmileEngine.cpp
@@ -104,6 +104,8 @@
 		}
 	}
 
+
+
 // Renderer
 	void Renderer::BeginScene(const Camera& camera, const DirectX::XMFLOAT4X4& cameraTransform)
 	{
@@ -132,4 +134,161 @@
 	void Renderer::Submit(const MeshRendererComponent& meshRendererComponent, const DirectX::XMFLOAT4X4& worldTransform)
 	{
 		Submit(meshRendererComponent.pVertexBuffer, meshRendererComponent.pIndexBuffer, meshRendererComponent.pShader, worldTransform);
+	}
+
+
+
+// Windows Window
+	WindowsWindow::WindowsWindow(const WindowSettings& settings)
+	{
+		Init(settings);
+	}
+
+	WindowsWindow::~WindowsWindow()
+	{
+		ShutDown();
+	}
+
+	void WindowsWindow::ShutDown()
+	{
+		DestroyWindow(m_WindowHandle);
+		delete m_pContext;
+	}
+
+	void WindowsWindow::Init(const WindowSettings& settings)
+	{
+		m_Data.Title = settings.Title;
+		m_Data.Height = settings.Height;
+		m_Data.Width = settings.Width;
+
+		m_Message = { 0 };
+
+		SM_LOG_INFO("WindowsWindow::Init > Creating window: %s (%d, %d)", settings.Title.c_str(), settings.Width, settings.Height);
+
+		// Create window class
+		const wchar_t* className = L"SmileWindowClass";
+		WNDCLASSEX windowClass;
+		windowClass.cbSize = sizeof(WNDCLASSEX);
+		windowClass.style = CS_HREDRAW | CS_VREDRAW;
+		windowClass.cbClsExtra = 0;
+		windowClass.cbWndExtra = 0;
+
+		windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+		windowClass.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+
+		windowClass.hIcon = static_cast<HICON>(LoadImage(HINSTANCE(),
+			MAKEINTRESOURCE(IDI_ICON1),
+			IMAGE_ICON,
+			256, 256,
+			LR_DEFAULTCOLOR));
+
+			/*LoadIcon(0, IDI_APPLICATION);*/
+		windowClass.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+		windowClass.lpszClassName = className;
+		windowClass.lpszMenuName = nullptr;
+
+		windowClass.hInstance = HINSTANCE();
+		windowClass.lpfnWndProc = WindowsProcedureStatic;
+
+		int success = RegisterClassEx(&windowClass);
+		SM_ASSERT(success, "Could not register window class!");
+
+		RECT windowRect{};
+		windowRect.left = 0;
+		windowRect.right = settings.Width + windowRect.left;
+		windowRect.top = 0;
+		windowRect.bottom = settings.Height + windowRect.top;
+		AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX, FALSE);
+
+		// Create and display the window
+		auto windowTitle = std::wstring{ settings.Title.begin(), settings.Title.end() };
+		m_WindowHandle = CreateWindow(className,
+				windowTitle.c_str(),
+				WS_OVERLAPPEDWINDOW | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				windowRect.right - windowRect.left,
+				windowRect.bottom - windowRect.top,
+				nullptr,
+				nullptr,
+				HINSTANCE(),
+				this);
+
+		SM_ASSERT(m_WindowHandle, "WindowsWindow::Init > Could not create window!")
+
+		// Init context
+		m_pContext = new DirectX11Context{ this };
+		m_pContext->Init();
+
+		ShowWindow(m_WindowHandle, SW_SHOW);
+		UpdateWindow(m_WindowHandle);
+		SM_LOG_INFO("WindowsWindow::Init > Window '%s' created", settings.Title.c_str());
+
+		SetVSync(true);
+		m_bInitialized = true;
+	}
+
+	void WindowsWindow::OnUpdate()
+	{
+		if (!m_bInitialized)
+			return;
+
+		PollEvents();
+		m_pContext->Present();
+	}
+
+	void WindowsWindow::PollEvents()
+	{
+		if (m_Message.message != WM_QUIT)
+		{
+			// If there are window messages then process them
+			if (PeekMessage(&m_Message, 0, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&m_Message);
+				DispatchMessage(&m_Message);
+			}
+		}
+	}
+
+
+// DirectX 11 Vetex Buffer
+	DirectX11VertexBuffer::DirectX11VertexBuffer(const VertexBufferData& vertexBufferData)
+		: m_Layout{ vertexBufferData.BufferLayout }
+	{
+		m_pDirectX11Context = static_cast<DirectX11Context*>(SmileGame::GetInstance().GetWindow().GetRenderingContext());
+
+		D3D11_BUFFER_DESC bd = {};
+		bd.Usage = BufferUsageToDirectXType(vertexBufferData.Usage);
+		bd.ByteWidth = vertexBufferData.BufferLayout.GetStride() * vertexBufferData.Count;
+		bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		bd.CPUAccessFlags = 0;
+		bd.MiscFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA initData = { 0 };
+		initData.pSysMem = vertexBufferData.pVertices;
+
+		HRESULT result = m_pDirectX11Context->GetDevice()->CreateBuffer(&bd, &initData, &m_pVertexBuffer);
+		if (FAILED(result))
+		{
+			SM_LOG_ERROR("DirectX11VertexBuffer > Failed to create vertex buffer");
+			return;
+		}
+	}
+
+	DirectX11VertexBuffer::~DirectX11VertexBuffer()
+	{
+		SAFE_RELEASE(m_pVertexBuffer);
+	}
+
+	void DirectX11VertexBuffer::Bind() const
+	{
+		uint32_t offset{ 0 };
+		uint32_t stride{ m_Layout.GetStride() };
+		m_pDirectX11Context->GetDeviceContext()->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &stride, &offset);
+	}
+
+	void DirectX11VertexBuffer::Unbind() const
+	{
+		m_pDirectX11Context->GetDeviceContext()->IASetVertexBuffers(0, 0, nullptr, nullptr, nullptr);
 	}
